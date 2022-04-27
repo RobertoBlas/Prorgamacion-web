6ee5ec5b596c937921da37a28049373f683c5b9b
RobertoBlas <104519627+RobertoBlas@users.noreply.github.com>
2022-04-27 11:46:50 -0500

    Archivos de la clase de Bucles.
    Aprendimos sobre los Bucles While y Do / While

---
+++ ciclos/code.js
@@ -0,0 +1,33 @@
+/*
+ ciclo while sirve para ejecurar un codigo de forma ciclica 
+ mienras su condición se cumpla
+ */
+
+/* 
+Definimos la variable i
+*/
+/*
+var i = 1;
+var numero = prompt("Elige un numero")
+while(i <= numero) {
+    /* 
+        Aquí va el codigo a ejecutar. 
+    */
+        /* 
+        document.write("Hola soy un ciclo :) <br>");
+        //sumamos una unidad a ka variable i, para que la condición deje de cumplirse//
+        // i = i + 1;
+        i ++;
+     } 
+     /*
+/*
+el ciclo do while ejecuta un codigo al menos 1 vez y si se cumple la condicion
+volvera a iterarlo
+*/
+var i = 1;
+var numero = prompt ("Elige un numero");
+do { 
+    //aqui el coidgo que se ejecuta en el ciclo.
+    document.write("Rob eres un tonto <br>");
+    i++;
+}while (i < numero);
